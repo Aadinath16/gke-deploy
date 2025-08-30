5890ecaaf124677640ebee5b0e3d551822d302c4
aadinath16 <adi.dhamdhere@yahoo.com>
2025-08-30 20:03:42 +0000

    code added

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import logging
 import sys
 
@@ -11,6 +11,25 @@ def create_app():
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s"
     )
+
+    logger = logging.getLogger(__name__)
+
+    @app.after_request
+    def after_request(response):
+        """Log every response with proper severity based on status code."""
+        status = response.status_code
+        msg = f"{request.method} {request.path} {status}"
+
+        if 200 <= status < 300:
+            logger.info(msg)
+        elif 400 <= status < 500:
+            logger.warning(msg)
+        elif 500 <= status < 600:
+            logger.error(msg)
+        else:
+            logger.info(msg)
+
+        return response
 
     from .main import register_routes
     register_routes(app)
