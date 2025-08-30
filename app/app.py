ace17a9dcb00c21e2a590f7dc7dc02dcf727289b
aadinath16 <adi.dhamdhere@yahoo.com>
2025-08-30 12:53:15 +0000

    added code

--- app/app.py
+++ app/app.py
@@ -1,39 +1,40 @@
-# app/main.py
+import logging
 from flask import Flask, jsonify
-import random
-import os
+import random, os
 
 app = Flask(__name__)
 
-# Use environment variable
+# Configure logging
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '[%(asctime)s] [%(levelname)s] %(message)s'
+)
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+
 APP_ENV = os.getenv("APP_ENV", "dev")
 
 @app.route("/")
 def home():
-    return jsonify({
-        "message": f"Hello from {APP_ENV} environment",
-        "status": "success"
-    }), 200  # Always success
+    app.logger.info("200 OK response for /")
+    return jsonify({"message": f"Hello from {APP_ENV}", "status": "success"}), 200
 
 @app.route("/random-status")
 def random_status():
-    """
-    Returns a random status code:
-      - 70% chance: 200 OK
-      - 20% chance: 500 Internal Server Error
-      - 10% chance: 404 Not Found
-    """
     rnd = random.random()
     if rnd < 0.7:
-        return jsonify({"message": "Everything is OK!", "status": "success"}), 200
+        app.logger.info("200 OK response for /random-status")
+        return jsonify({"message": "Everything OK!", "status": "success"}), 200
     elif rnd < 0.9:
+        app.logger.error("500 Internal Server Error simulated")
         return jsonify({"message": "Internal Server Error", "status": "error"}), 500
     else:
+        app.logger.warning("404 Not Found simulated")
         return jsonify({"message": "Not Found", "status": "error"}), 404
 
 @app.route("/health")
 def health():
+    app.logger.info("Health check OK")
     return jsonify({"status": "healthy"}), 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
