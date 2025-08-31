3e39223c00bf974b42c50e35713dec04ed76ec45
aadinath16 <adi.dhamdhere@yahoo.com>
2025-08-31 14:54:46 +0000

    added code

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -8,6 +8,6 @@ resource "google_compute_subnetwork" "subnet" {
   name          = "${var.environment}-subnet"
   ip_cidr_range = var.subnet_cidr
   region        = var.region
-  network       = google_compute_network.vpc.id
+  network       = google_compute_network.vpc_network.id
   project       = "${var.project_id}-${var.environment}"
 }
